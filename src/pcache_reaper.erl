c8c7a58080ace79b13566f527026c5e0dbed339d
Led <ledest@gmail.com>
2016-01-24 00:16:12 +0200

    Remove double check of cache size

--- src/pcache_reaper.erl
+++ src/pcache_reaper.erl
@@ -35,15 +35,7 @@ shrink_cache_to_size(Name, _CurrentCacheSize, CacheSize) ->
 pcache_reaper(Name, CacheSize) ->
   % sleep for 4 seconds
   timer:sleep(4000),
-  % Lame.  Account for sizes better.  total_size asks every datum for its size.
-  CurrentCacheSize = pcache:total_size(Name),
-  if
-    CurrentCacheSize < CacheSize -> ok;
-    CurrentCacheSize >= CacheSize ->
-%io:format("Cache ~p too big!  Shrinking...~n", [self()]),
-%io:format("CurrentSize: ~p; Target Size: ~p~n", [CurrentCacheSize, CacheSize]),
-      shrink_cache_to_size(Name, CurrentCacheSize, CacheSize)
-  end,
+  shrink_cache_to_size(Name, pcache:total_size(Name), CacheSize),
   pcache_reaper(Name, CacheSize).
     
 init([Name, CacheSizeBytes]) ->
